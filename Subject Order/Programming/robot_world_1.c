26d687bf3814bd1d8301c1337d25ebb6d966d03b
anna757 <72200944+anna757@users.noreply.github.com>
2021-06-02 01:12:39 +0200

    programming task complete

--- Subject Order/Programming/robot_world_1.c
+++ Subject Order/Programming/robot_world_1.c
@@ -28,10 +28,103 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <stdio.h>
 #include <stdlib.h>
 
-// THIS IS THE FUNCTION YOU IMPLEMENT
 int move(char *world) {
-    // YOUR CODE HERE
-    return 1; // REPLACE THE RETURN VALUE WITH YOUR CALCULATED RETURN VALUE
+    // copy the array into a new one to get the size
+    char world2[200];
+    for(int i=0;i<200;i++) {
+        world2[i] =world[i];
+    }
+
+    unsigned int elements = sizeof(world2)/sizeof(world2[0]);
+    int robot_index;
+    int target_index;
+    int lines=0;
+
+    for(int i = 0; i < elements; ++i) { //index of R
+        if (world2[i] == 'R') {
+            robot_index = i;
+            break;
+        }
+    }
+
+    for(int i = 0; i < elements; ++i) { //index of T
+        if (world2[i] == 'T') {
+            target_index = i;
+            break;
+        }
+    }
+	 
+    //if T has bigger position index than R in the array
+    if(robot_index < target_index){ 
+        //distance between indexes of R and T
+        int distance = target_index - robot_index;	 
+
+        //count how many lines between R and T
+        for(int i = robot_index; i < target_index; ++i) { 
+            if (world2[i] == '\n') {
+                lines++;
+            }
+        }
+
+        //as long as R is above T, go down, until it reaches the same line.
+        do {  
+            for (int i=0; i<lines;i++) {
+                return 3;
+	        }       
+        }
+        while (lines > 0) ; 
+        
+        //while R is on same line of T, go right.
+        do {
+            for (int i=0; i<distance;i++) {
+	            return 2;
+            }
+        }
+        while (lines < 1 && distance > 0);
+
+        //while R is on the same line of T, go left.
+        do  {
+            for (int i=0; i<distance;i++) {
+	            return 4;
+            }
+	    }
+        while (lines < 1 && distance < 0);
+    }
+
+    //if T has smaller position index than R in the array
+    else if (robot_index > target_index){
+        //distance between indexes of R and T
+        int distance = robot_index - target_index;
+        //count how many lines between R and T
+        for(int i = target_index; i < robot_index; ++i) { 
+            if (world2[i] == '\n') {
+                lines++;
+            }
+        }
+
+        do { 
+            for (int i=0; i<lines;i++) {
+                return 1;
+	        }       
+        }
+        while (lines > 0) ; 
+    
+        //while R is on the same line of T, go left
+        do {
+            for (int i=0; i<distance;i++) {
+	            return 4;
+            }
+        }
+        while (lines < 1 && distance > 0);
+
+        //while R is on same line of T, go right.
+        do  {
+            for (int i=0; i<distance;i++) {
+	            return 2;
+            }
+	    }
+        while (lines < 1 && distance < 0);
+    }
 }
 
 // Return target index
@@ -71,7 +164,7 @@ int update_world(int movement, char *world, int robot_index, int width) {
         world[robot_index] = 'O';
         printf("%s", world);
         printf("%c", '\n');
-        printf("SUCCESS, target found!");
+        printf("GREAT SUCCESS, target found!");
         exit(0);
     }
 }
@@ -106,17 +199,17 @@ int main() {
 
     for(int i = 0; i < elements; ++i) {
     if (world[i] == 'R') {
-            robot_index = i;
-            break;
+        robot_index = i;
+        break;
         }
     }
     for(int i = 0; i < elements; ++i) {
     if (world[i] == 'T') {
-            target_index = i;
-            break;
+        target_index = i;
+        break;
         }
     }
-
+       
     // Print the initial world
     printf("Starting position: %c", '\n');
     // Debug output
